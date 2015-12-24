9f8880f2e8fe7a6bca60c4c89f73cb58302088f5
Dmitriy Olshevskiy <olshevskiy87@bk.ru>
2015-12-24 19:20:49 +0200

    change var names

--- check_digits--1.0.sql
+++ check_digits--1.0.sql
@@ -1,6 +1,6 @@
 \echo Use "CREATE EXTENSION check_digits" to load this file. \quit
 
-CREATE FUNCTION chdig_inn(t text)
+CREATE FUNCTION check_digits_inn(inn text)
 RETURNS integer
 AS 'MODULE_PATHNAME'
 LANGUAGE C STRICT;
--- check_digits.c
+++ check_digits.c
@@ -9,33 +9,34 @@
 PG_MODULE_MAGIC;
 #endif
 
-PG_FUNCTION_INFO_V1(chdig_inn);
+PG_FUNCTION_INFO_V1(check_digits_inn);
 
-Datum chdig_inn(PG_FUNCTION_ARGS) {
-    text    *t;
-    int      t_len, i;
-    char    *c_t;
+Datum
+check_digits_inn(PG_FUNCTION_ARGS) {
+    text    *inn;
+    int      inn_len, i;
+    char    *c_inn;
 
     if (PG_ARGISNULL(0)) {
         PG_RETURN_NULL();
     }
 
-    t = PG_GETARG_TEXT_P(0);
-    t_len = VARSIZE(t) - VARHDRSZ;
-    if (t_len != 10 && t_len != 12) {
+    inn = PG_GETARG_TEXT_P(0);
+    inn_len = VARSIZE(inn) - VARHDRSZ;
+    if (inn_len != 10 && inn_len != 12) {
         ereport(ERROR,
                 (errcode(ERRCODE_STRING_DATA_LENGTH_MISMATCH),
                  errmsg("INN must contain 10 or 12 digits")));
     }
-    c_t = text_to_cstring(t);
-    for (i = 0; i < t_len; i++) {
-        if (!isdigit(c_t[i])) {
+    c_inn = text_to_cstring(inn);
+    for (i = 0; i < inn_len; i++) {
+        if (!isdigit(c_inn[i])) {
             ereport(ERROR,
                     (errcode(ERRCODE_DATA_EXCEPTION),
-                     errmsg("invalid symbol \"%c\"", c_t[i])));
+                     errmsg("invalid symbol \"%c\"", c_inn[i])));
         }
     }
-    pfree(c_t);
+    pfree(c_inn);
 
-    PG_RETURN_INT32(t_len);
+    PG_RETURN_INT32(inn_len);
 }
